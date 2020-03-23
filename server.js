c7d74f5acbbe7f6b89ede18f1f56df35fa843b79
Zac Eskridge <zace118@gmail.com>
2020-03-23 01:43:58 -0700

    Added in more files to help signup, login, etc.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const session = require("express-session");
 // Requiring passport as we've configured it
-const passport = require("./config/passport");
+// const passport = require("./config/passport");
 
 const dotenv = require("dotenv");
 console.log(process.env.SECRET_MESSAGE);
