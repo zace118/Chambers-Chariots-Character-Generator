97b55bee1b7d7256f7a49e3fb48e62ab6f64cce2
Zac Eskridge <zace118@gmail.com>
2020-03-23 13:42:06 -0700

    Did a little spring cleaning on my files and removed some things

--- db/seeds.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO characters (name, race, class, level, hp)
-VALUES ('Sir Fitzroy Maplecourt', 'Human', 'Barbarian', 4, 30);
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -9,23 +9,23 @@ module.exports = function (app) {
     app.get("/", function (req, res) {
         // If the user already has an account send them to the members page
         if (req.user) {
-            res.redirect("/members");
+            res.redirect("/profile");
         }
-        res.sendFile(path.join(__dirname, "../public/signup.html"));
+        res.sendFile(path.join(__dirname, "../public/index"));
     });
 
     app.get("/login", function (req, res) {
         // If the user already has an account send them to the members page
         if (req.user) {
-            res.redirect("/members");
+            res.redirect("/profile");
         }
-        res.sendFile(path.join(__dirname, "../public/login.html"));
+        res.sendFile(path.join(__dirname, "../public/login"));
     });
 
     // Here we've add our isAuthenticated middleware to this route.
     // If a user who is not logged in tries to access this route they will be redirected to the signup page
-    app.get("/members", isAuthenticated, function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/profile.html"));
+    app.get("/profile", isAuthenticated, function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/profile"));
     });
 
     // app.get("/viewMeetups", isAuthenticated, function (req, res) {
