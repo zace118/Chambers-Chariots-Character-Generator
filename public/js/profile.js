d2043a94c08a7af38ddbf1cfe29394167df34977
Zac Eskridge <zace118@gmail.com>
2020-03-24 16:01:45 -0700

    Fixed the welcome message to display the correct, logged in user.

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
-    // // appends the username to the welcome back thing.
-    // $("#welcomeBack").append()
+    // This file just does a GET request to figure out which user is logged in
+    // and updates the HTML on the page
+    $.get("/api/user_data").then(function (data) {
+        $(".member-name").text(data.name);
+    });
 
-    // // `Welcome back, ${userName}!`
 
     // --------------------------------------------------
+
+
     $.get("/api/post_character", function (characters) {
         console.log(characters);
 
